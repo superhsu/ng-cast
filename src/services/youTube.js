9c5fd53d63794b1bc5f55d6cbf520687cb045278
superhsu <whoisandrewjhsu@gmail.com>
2018-02-03 11:07:36 -0800

    GET request working

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,6 +1,45 @@
 angular.module('video-player')
-  .service('youTube', function() {
-    this.searchService = function () {
-      console.log('searched!');
+  .service('youTube', function($http) {
+    this.searchService = function (query) {
+      $http({
+        method: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        params: {
+          q: 'cats',
+          max: 5,
+          type: 'video',
+          videoEmbeddable: 'true',
+          key: window.YOUTUBE_API_KEY,
+          part: 'snippet'
+        }
+      }).then(function successCallback(data) {
+        console.log('searched!');
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(data) {
+        console.log('err');
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
     };
   });
+
+
+
+// $http.get('https://www.googleapis.com/youtube/v3/search',
+//         {q: 'cats',
+//         max: 5,
+//         type: 'video',
+//         key: window.YOUTUBE_API_KEY,
+//         part: 'snippet',
+//         videoEmbeddable: 'true'})
+//         .then(function successCallback(data) {
+//         console.log('searched!');
+//         // this callback will be called asynchronously
+//         // when the response is available
+//       }, function errorCallback(data) {
+//         console.log('err', data);
+//         // called asynchronously if an error occurs
+//         // or server returns response with an error status.
+//       });
